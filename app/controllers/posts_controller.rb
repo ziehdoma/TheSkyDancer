4d99c741a011e774dc120659a48a941aed80d18e
Dhamzee Sherpa <dhamtsik@gmail.com>
2016-01-30 20:57:09 -0500

    Initial Commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+  def home
+  end
+
+  def index
+  	@posts = Post.all
+  end 
+
+  def new
+  	@post = Post.new
+  end 
+
+
+  def create
+  	@post = Post.new(post_params)
+  	if @post.save
+  		redirect_to blogs_path(@post), alert: "Post Created!"
+  	else 
+  		render :new
+  	end 
+  end 
+
+  def show
+  	set_params
+  end 
+
+  def edit
+  	set_params
+  	redirect_to blog_path
+  end 
+
+  def destroy
+  	set_params
+  	@post.destroy
+  	redirect_to root_path, alert: "Post deleted"
+  end 
+
+
+  private 
+  def post_params
+  	params.require(:post).permit(:title, :body, :author)
+  end 
+
+  def set_params
+  	@post = Post.find(params[:id])
+  end 
+
+end
